7eaf04dc3a701dd091dd5259b6f3cff3d034d180
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-06-06 00:51:44 -0300

    new test

--- resources/views/layouts/formtemplate.blade.php
+++ resources/views/layouts/formtemplate.blade.php
@@ -19,6 +19,9 @@
     <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
     
     @yield('custom_css')
+    @push('styles')
+    <link rel="stylesheet" href="{{ asset('css/inscription.css') }}">
+@endpush
     <script type="application/ld+json">{
 		"@context": "http://schema.org",
 		"@type": "Organization",
@@ -39,7 +42,7 @@
         </div>
       </div>
     </header> --}}
-    <section class=" body-insc" id="sec-3ffd">
+    <section class="body-insc" id="sec-3ffd">
     <div class="">
         <div class="">
             <div class="">
