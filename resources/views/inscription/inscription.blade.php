ee880c0fb1492c488937e9acc8b020ab64d959ff
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-06-07 22:42:13 -0300

    deleting some divs

--- resources/views/inscription/inscription.blade.php
+++ resources/views/inscription/inscription.blade.php
@@ -50,9 +50,9 @@ transformación Educativa">
            
                   <div class="u-container-style u-layout-cell u-opacity u-opacity-85 u-shape-rectangle u-size-30 u-layout-cell-2">
                    
-                  <div class="u-container-style u-layout-cell u-opacity u-opacity-85 u-shape-rectangle u-size-30 u-layout-cell-3">
+                  {{-- <div class="u-container-style u-layout-cell u-opacity u-opacity-85 u-shape-rectangle u-size-30 u-layout-cell-3">
                     <div class="u-container-layout u-container-layout-4">
-                      <div class="u-form u-form-1" style="display:flex; flex-direction:column; align-items:center">
+                      <div class="u-form u-form-1" style="display:flex; flex-direction:column; align-items:center"> --}}
                       
     {{-- <input type="hidden" id="old_first_workshop_group_id" value={{ old('first_workshop_group_id') ?? 0 }}>
     <input type="hidden" id="old_second_workshop_group_id" value={{ old('second_workshop_group_id') ?? 0 }}> --}}
@@ -152,9 +152,9 @@ transformación Educativa">
 
                       </div>
                     </div>
-                  </div>
+                  {{-- </div>
                 </div>
-              </div>
+              </div> --}}
           
         </div>
       </div>
