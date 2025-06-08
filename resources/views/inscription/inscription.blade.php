88ac7a43c79197e307814c8d28132e91e4475fe2
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-06-07 21:43:59 -0300

    fix style

--- resources/views/inscription/inscription.blade.php
+++ resources/views/inscription/inscription.blade.php
@@ -31,7 +31,7 @@ transformación Educativa">
     <meta property="og:type" content="website">
   <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
   <body data-path-to-root="./" data-include-products="false" class="u-body u-overlap u-xl-mode" data-lang="es"><header class="u-clearfix u-container-align-center u-custom-color-26 u-header u-sticky u-sticky-387b u-header" id="sec-055d"><div class="u-clearfix u-sheet u-sheet-1">
-        <img class="u-align-center u-image u-image-default u-image-1" src="images/logo.png" alt="" data-image-width="1024" data-image-height="117" data-lang-es="">
+        <img class="u-align-center u-image u-image-default u-image-1" src="images/logo.png" alt="" data-image-width="1024" data-image-height="117" data-lang-es="" style="margin-top:20px;">
       </div></header>
     <section class="u-clearfix u-section-1" id="sec-3ffd">
       <div class="u-clearfix u-sheet u-sheet-1">
@@ -149,7 +149,7 @@ transformación Educativa">
                 <div style="width: 100%;text-align: center;">                                                  </div>
             </div>
         </div>
-        <div style="margin-top:10px;">
+        <div style="margin-top:20px;">
          {{-- <div class="u-align-right u-form-group u-form-submit"> --}}
                 <a href="https://lamennais.edu.uy/cp25" 
                 class="button-save">Volver</a>
