ce986320e4f68bc218b4a1a3a5be21f701a1ec17
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-06-09 14:58:06 -0300

    changing year of certificate

--- resources/views/pdf/certificate.blade.php
+++ resources/views/pdf/certificate.blade.php
@@ -16,7 +16,7 @@
 </head>
 <body style="width: 1024px; font-size:20px; font-family:'Open Sans',sans-serif"; >
     <div style="width:934px; margin-top:280px; text-align:right">
-        Montevideo, viernes 26 de julio de 2024
+        Montevideo, viernes 26 de julio de 2025
         </div>
     <div style="margin-left:80px; margin-top:49px">
         Se certifica que:
