6f90f86fb3503a4e08de857fc209a6255647f732
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-08-04 16:23:44 -0300

    adding info to the mail

--- resources/views/emails/recoverycertificate.blade.php
+++ resources/views/emails/recoverycertificate.blade.php
@@ -4,9 +4,11 @@
 <p>
     En el siguiente enlace puede descargarse su  certificado de asistencia: <a href="{!!$inscription->certificateUrl()!!}">{!!$inscription->certificateUrl()!!}</a>
 </p>
-{{-- <p>
-    También compartimos los materiales que nos hicieron llegar los oradores: <a href=" https://audec.edu.uy/index.php/category/materiales/"> https://audec.edu.uy/index.php/category/materiales/</a>
+<p>
+Le compartimos el material de los expositores: <a href="  https://tinyurl.com/CongresoPedagogico"> https://tinyurl.com/CongresoPedagogico</a>
 </p>
+{{-- 
+
 <p>
    A partir de la semana próxima encontrarán las grabaciones de los encuentros Zoom en nuestro canal de Youtube <a href=" https://www.youtube.com/@audec-asociacionuruguayade8401"> https://www.youtube.com/@audec-asociacionuruguayade8401</a>
 </p> --}}
